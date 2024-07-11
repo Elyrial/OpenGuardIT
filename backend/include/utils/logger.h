442a9eed4b5a3197ba675de1e7497040cda76625
Elyrial <alexpersson@proton.me>
2024-07-11 21:35:21 +0200

    Initial commit for logger functions

---
+++ backend/include/utils/logger.h
@@ -0,0 +1,9 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+// include spdlog header
+
+
+void init_logging();
+
+#endif
---
+++ backend/src/utils/logger.cpp
@@ -0,0 +1,8 @@
+#include "logger.h"
+// add spdlog basic file sink header
+
+void init_logging() {
+    auto file_logger = spdlogo::basic_loggermt("file_logger", "logs/backend.log");
+    spdlog::set_default_logger(file_logger);
+    spdlog::set_level(spdlog::level::info);
+}
