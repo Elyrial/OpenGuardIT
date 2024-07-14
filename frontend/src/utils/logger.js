efebdb5ebeabe538db013b7ccdfbb8a2e71e5d59
Elyrial <alexpersson@proton.me>
2024-07-14 21:45:44 +0200

    Added temporary logtester for frontend with console logging

---
+++ frontend/src/utils/logger.js
@@ -0,0 +1,10 @@
+
+// Test examples
+export const logInfo = (message) => {
+    console.log(`[INFO] ${messaage}`);
+}
+
+
+export const logError = (message) => {
+    console.error(`[ERROR] ${messaage}`);
+}
